ce2f871f84fc15f1bf08305401ccd23f3320a4a0
Raju Jadhav <rajujadhav6343@gmail.com>
2024-03-13 17:59:30 +0530

    First commit

---
+++ main.js
@@ -0,0 +1,38 @@
+//To Start Setup
+//npm create vite@latest
+//npm install
+// cd project-file
+//npm install three
+
+//Deploy
+//npm run build or npx vite build
+//git init
+//git add .
+//git branch -M main
+//git remote add origin https://github.com.....
+//git push -u origin main
+
+import "./style.css";
+import javascriptLogo from "./javascript.svg";
+import viteLogo from "/vite.svg";
+import { setupCounter } from "./counter.js";
+
+document.querySelector("#app").innerHTML = `
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+    </a>
+    <h1>Hello Vite!</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite logo to learn more
+    </p>
+  </div>
+`;
+
+setupCounter(document.querySelector("#counter"));
