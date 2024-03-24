bc2b22f67f161f0ba989cbc591d2378e1e9b4d92
rajujadhav6343 <99895348+rajujadhav6343@users.noreply.github.com>
2024-03-24 15:14:11 +0530

    Update main.js

--- main.js
+++ main.js
@@ -8,6 +8,7 @@
 //npm run build or npx vite build
 //git init
 //git add .
+//git commit -m "commit message..."
 //git branch -M main
 //git remote add origin https://github.com.....
 //git push -u origin main or git push -f origin main
